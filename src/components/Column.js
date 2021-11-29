447958bd0c88ae2b6e4874abcf97c625da57d915
OussamaIdbid <57132225+OussamaIdbid@users.noreply.github.com>
2021-11-29 15:53:13 +0100

    drag and drop from column to other column

--- src/components/Column.js
+++ src/components/Column.js
@@ -5,25 +5,26 @@ import { Droppable } from "react-beautiful-dnd";
 const Column = (props) => {
   const { column, tasks } = props;
 
-  console.log(tasks);
   return (
     <div className="container">
       <div className="header">
         <h1>{column.title}</h1>
       </div>
       <div className="task-container">
-          {tasks.length > 0 ? 
         <Droppable droppableId={column.id}>
           {(provided) => (
             <div ref={provided.innerRef} {...provided.droppableProps}>
-              {tasks.map((task, index) => (
-                <Task key={task.id} task={task} index={index} />
-              ))}
+              {tasks.length > 0 ? (
+                tasks.map((task, index) => (
+                  <Task key={task.id} task={task} index={index} />
+                ))
+              ) : (
+                <h1 className="no-item">No Goals yet assigned to this Level</h1>
+              )}
               {provided.placeholder}
             </div>
           )}
         </Droppable>
-        : <h1 className="no-item">No Goals yet assigned to this Level</h1>}
       </div>
     </div>
   );
--- src/components/DragDrop.js
+++ src/components/DragDrop.js
@@ -27,30 +27,60 @@ const DragDrop = () => {
       return;
     }
 
-    const column = initialData.columns[source.droppableId];
-    const newTaskIds = Array.from(column.taskIds);
-    newTaskIds.splice(source.index, 1);
-    newTaskIds.splice(destination.index, 0, draggableId);
+    if (destination.droppableId !== source.droppableId) {
+      const destinationColumn = initialData.columns[destination.droppableId];
+      const column = initialData.columns[source.droppableId];
+      const newTaskIds = Array.from(column.taskIds);
+      const newTaskIdsDestination = Array.from(destinationColumn.taskIds);
+      newTaskIds.splice(source.index, 1);
+      newTaskIdsDestination.splice(destination.index, 0, draggableId);
 
-    const newColumn = {
-      ...column,
-      taskIds: newTaskIds,
-    };
+      const newColumn = {
+        ...column,
+        taskIds: newTaskIds,
+      };
 
-    const newState = {
+      const destColumn = {
+        ...destinationColumn,
+        taskIds: newTaskIdsDestination,
+      };
+
+      console.log(newColumn);
+      const newState = {
         ...initialData,
         columns: {
-            ...initialData.columns,
-            [newColumn.id]: newColumn,
+          ...initialData.columns,
+          [newColumn.id]: newColumn,
+          [destColumn.id]: destColumn,
         },
+      };
+      setInitialData(newState);
+    } else {
+      const column = initialData.columns[source.droppableId];
+      const newTaskIds = Array.from(column.taskIds);
+      newTaskIds.splice(source.index, 1);
+      newTaskIds.splice(destination.index, 0, draggableId);
+
+      const newColumn = {
+        ...column,
+        taskIds: newTaskIds,
+      };
+
+      console.log(newColumn);
+      const newState = {
+        ...initialData,
+        columns: {
+          ...initialData.columns,
+          [newColumn.id]: newColumn,
+        },
+      };
+      setInitialData(newState);
     }
-    setInitialData(newState);
   };
 
   const data = () => {
     if (Object.keys(initialData).length === 0) return;
 
-    console.log(initialData);
     return (
       <DragDropContext onDragEnd={onDragEnd}>
         {initialData.columnorder.map((columnId) => {
